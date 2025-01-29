08c9b04649f7dd728c67150e32bd5a0ee48cf343
AlinessSantos <alinesantosluz20@gmail.com>
2025-01-29 18:39:56 -0300

    final with comments

--- TPF/data_colletor.cpp
+++ TPF/data_colletor.cpp
@@ -21,56 +21,76 @@ std::string getCurrentTimestamp() {
     auto now_time_t = std::chrono::system_clock::to_time_t(now);
     std::ostringstream oss;
     oss << std::put_time(std::gmtime(&now_time_t), "%Y-%m-%dT%H:%M:%SZ");
-    //teste timestamp
-    //std::string s = oss.str();
-    //std::cout << s << std::endl;
     return oss.str();
 }
 
-// Função para receber os dados da resposta HTTP
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
+// Função para processar os dados da resposta HTTP e adicionar à string de origem
+// bufferptr aponta para o buffer onde os dados serao armazenados (readBuffer)
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* bufferptr) {
+    ((std::string*)bufferptr)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
-//teste commit
-// Função para pegar os dados da API OpenWeatherMap
+// Função para pegar os dados da API OpenWeatherMap, chave, cidade, país
 std::string getWeatherData(const std::string& apiKey, const std::string& city, const std::string& country) {
+    //ponteiro para manipular o cliente http, biblioteca cURL
     CURL* curl;
+    //codigo de retorno de falha ou sucesso
     CURLcode res;
+    //armazena os dados recebidos da resposta http da api
     std::string readBuffer;
 
+    //inicializa a biblioteca cURL no modo default
     curl_global_init(CURL_GLOBAL_DEFAULT);
+    //inicializa o manipulador cURL para executar a solicitação http
     curl = curl_easy_init();
 
+    //verifica a inicialização do manipulador
     if (curl) {
-        std::string url = "http://api.openweathermap.org/data/2.5/weather?q=Belo%20Horizonte,BR&appid=21309ecc4422778de48b2f48e31143cb&units=metric";
+        //url da API, metric: graus celsius
+        std::string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid=" + apiKey + "&units=metric";
+        //configura o manipulador para usar a url gerada (c_str converte string para const char*)
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        //Define uma função de retorno (WriteCallback) para ser chamada sempre que os dados forem recebidos da resposta HTTP.
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        //define o destino dos dados de writeCallBack
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
         
+        //executa a solicitação http e armazena em res o codigo de sucesso
         res = curl_easy_perform(curl);
         
+        //Verifica se a solicitação foi bem-sucedida.
         if (res != CURLE_OK) {
             std::cerr << "CURL request failed: " << curl_easy_strerror(res) << std::endl;
         }
 
+        //libera o manipulador
         curl_easy_cleanup(curl);
     }
 
+    //finaliza a biblioteca cURL
     curl_global_cleanup();
     //testa readBUFFER
-    //std::cout << readBuffer << std::endl;
+    std::cout << readBuffer << std::endl;
+    //retorna os dados JSON da api
     return readBuffer;
 }
 
 // Função para processar os dados JSON e extrair temperatura e umidade
+//data-> referencia para a string que contem os dados json da api
+//temperature-> referencia para a variavel onde a temperatura sera armazenada
+//humidity -> referencia para a variavel onde a umidade sera armazenada
 void processWeatherData(const std::string& data, float& temperature, float& humidity) {
+    //inicializa um objeto charReaderBuilderusado para configurar e criar um leitor de JSON
     Json::CharReaderBuilder reader;
+    //estrutura de dados, objeto json generico (root = base) para armazenar dados extraidos da string json, acessado como um map
     Json::Value root;
+    //converte a string data em um formato que pode ser lido sequencialmente, como se fosse um arquivo de entrada.
     std::istringstream s(data);
+    //armazena mensagem de erro
     std::string errs;
-    
+    //converte a string s em um objeto value root, reader configura o parser
     if (Json::parseFromStream(reader, s, &root, &errs)) {
+        //acessa as propriedades dentro do root e converte para float, salvando nas variaveis de referencia
         temperature = root["main"]["temp"].asFloat();
         humidity = root["main"]["humidity"].asFloat();
     } else {
@@ -83,6 +103,7 @@ void publishInitialMessage(mqtt::async_client& client) {
     Json::Value root;
     root["machine_id"] = MACHINE_ID;
     
+    //cria sensores
     Json::Value sensor1;
     sensor1["sensor_id"] = SENSOR_ID_TEMPERATURE;
     sensor1["data_type"] = "float";
@@ -93,23 +114,32 @@ void publishInitialMessage(mqtt::async_client& client) {
     sensor2["data_type"] = "float";
     sensor2["data_interval"] = DATA_INTERVAL;
 
+    //adiciona cada sensor ao campo sensors (array)
     root["sensors"][0] = sensor1;
     root["sensors"][1] = sensor2;
 
+    //inicializa um ojeto streamWriterBuilder que converte o ojbeto json root em string formato json
     Json::StreamWriterBuilder writer;
+    //convertendo... e armazenando em message
     std::string message = Json::writeString(writer, root);
     //testa mensagem inicial
     std::cout << message << std::endl;
+    //cria a mensagem mqtt no topico /sensor_monitors, conteudo: message, e armazena em msg
     mqtt::message_ptr msg = mqtt::make_message("/sensor_monitors", message);
+    //publica a msg no topico pelo cliente client (data_colletor)
     client.publish(msg)->wait_for(std::chrono::seconds(10));
 }
 
 int main(int argc, char* argv[]) {
+    //inicializa o cliente mqtt assíncrono
     mqtt::async_client client(SERVER_ADDRESS, CLIENT_ID);
 
     // Conectando ao broker
+    //objeto de opções de conexão para configurar a conexão entre o client e o broker mqtt
     mqtt::connect_options connOpts;
+    //inicia a conexão com o broker mqtt
     mqtt::token_ptr conntok = client.connect(connOpts);
+    //bloqueia a execução até estabelezar a conexão
     conntok->wait();
 
     // Publicando mensagem inicial
@@ -117,9 +147,14 @@ int main(int argc, char* argv[]) {
 
     while (true) {
         // Pegando os dados da API
-        std::string weatherData = getWeatherData("21309ecc4422778de48b2f48e31143cb", "Belo Horizonte", "BR");
+        std::string weatherData = getWeatherData("21309ecc4422778de48b2f48e31143cb", "Belo%20Horizonte", "BR");
+        //std::string weatherData = getWeatherData("21309ecc4422778de48b2f48e31143cb", "Beijing", "CN");
+        //std::string weatherData = getWeatherData("21309ecc4422778de48b2f48e31143cb", "London", "GB");
+        //std::string weatherData = getWeatherData("21309ecc4422778de48b2f48e31143cb", "New%20York", "US");
         
+        //verifica se a string está vazia
         if (!weatherData.empty()) {
+            //declara as variaveis onde os dados serao armazenados
             float temperature = 0.0f;
             float humidity = 0.0f;
             
@@ -135,16 +170,17 @@ int main(int argc, char* argv[]) {
             humMsg["timestamp"] = getCurrentTimestamp();
             humMsg["value"] = humidity;
 
+            //converte o objeto json em string formato json
             Json::StreamWriterBuilder writer;
             std::string tempMessage = Json::writeString(writer, tempMsg);
             std::string humMessage = Json::writeString(writer, humMsg);
-            //testa impressao
-            std::cout << "temp: " << tempMessage << std::endl << "umidade: " << humMessage << std::endl; 
+            //testa publicação
+            std::cout << "temperature: " << tempMessage << std::endl << "umidity: " << humMessage << std::endl; 
             // Publicando as mensagens no MQTT
             client.publish(mqtt::make_message("/sensors/" + MACHINE_ID + "/" + SENSOR_ID_TEMPERATURE, tempMessage))->wait_for(std::chrono::seconds(10));
             client.publish(mqtt::make_message("/sensors/" + MACHINE_ID + "/" + SENSOR_ID_HUMIDITY, humMessage))->wait_for(std::chrono::seconds(10));
             
-            std::cout << "Published temperature and humidity." << std::endl;
+            std::cout << "Published temperature and humidity. "<< std::endl;
         }
 
         // Aguardar antes de coletar novamente
--- TPF/data_processor.cpp
+++ TPF/data_processor.cpp
@@ -10,15 +10,20 @@
 #include <iomanip>
 #include <sqlite3.h>
 
+//onde o cliente se conectará, broker roda ocalmente na porta 1883
 const std::string SERVER_ADDRESS("tcp://localhost:1883");
+//ID único do cliente MQTT
 const std::string CLIENT_ID("DataProcessorClient");
+//máquina que está enviando os dados, deveríamos pegar do json??
 const std::string MACHINE_ID("machine_01");
-const int DATA_INTERVAL = 10; // 30 minutos em segundos
+//intervalo de tempo entre verificações de sensores inativos
+const int DATA_INTERVAL = 10; // em segundos
 
 // Conexão com banco de dados SQLite
 sqlite3* db;
-
+//retorna um int de sucesso ou erro na criação da tabela
 int createTables() {
+    //string de comando em SQL para criar a tabela, R"()" é string raw de c++
     const char* createSensorsTableSQL = R"(
         CREATE TABLE IF NOT EXISTS sensor_data (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -37,10 +42,13 @@ int createTables() {
             timestamp TEXT
         );
     )";
-    
+    //armazena mensagens de erro
     char* errorMessage;
+    //executa o comando char de criação de tabela em SQL, erro armazenado na variavel
     if (sqlite3_exec(db, createSensorsTableSQL, 0, 0, &errorMessage) != SQLITE_OK) {
+        //mensagem de erro é exibida no stderr (fluxo de erro padrão)
         std::cerr << "Error creating sensor_data table: " << errorMessage << std::endl;
+        //libera memória da variavel do tipo ponteiro
         sqlite3_free(errorMessage);
         return -1;
     }
@@ -58,33 +66,34 @@ int createTables() {
 struct SensorData {
     float value;
     std::string timestamp;
+    //Quantidade de períodos (T=10s???) sem receber dados do sensor
     int missed_periods = 0;
 };
 
 // Classe para gerenciar alarmes
-class AlarmManager {
-public:
-    void generateAlarm(const std::string& alarmMessage) {
-        std::cout << "ALARM: " << alarmMessage << std::endl;
-        // Persistir no banco de dados (simulação)
-    }
-};
+// retorna um inteiro de sucesso ou falha na inserção de uma nova leitura de sensor na tabela sensor_data
 int insertSensorData(const std::string& machineId, const std::string& sensorId, float value, const std::string& timestamp) {
+    //Comando em SQL para interir o registro na tabela (? são placeholders = espaço reservado)
     std::string sql = "INSERT INTO sensor_data (machine_id, sensor_id, value, timestamp) VALUES (?, ?, ?, ?);";
+    //Ponteiro statement = uma declaração SQL compilada que será executada no SQLite.
     sqlite3_stmt* stmt;
     
+    //Compila string SQL no stmt a ser executada no banco db (c_str converte string para const char*)
     if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, 0) != SQLITE_OK) {
         std::cerr << "Error preparing statement: " << sqlite3_errmsg(db) << std::endl;
         return -1;
     }
     
+    //Comandos que substituem os placeholder "?" pelos valores (-1 calcula o tamanho da string automaticamente)
     sqlite3_bind_text(stmt, 1, machineId.c_str(), -1, SQLITE_STATIC);
     sqlite3_bind_text(stmt, 2, sensorId.c_str(), -1, SQLITE_STATIC);
     sqlite3_bind_double(stmt, 3, value);
     sqlite3_bind_text(stmt, 4, timestamp.c_str(), -1, SQLITE_STATIC);
     
+    //executa stmt e retorna se foi bem sucedida
     if (sqlite3_step(stmt) != SQLITE_DONE) {
         std::cerr << "Error executing statement: " << sqlite3_errmsg(db) << std::endl;
+        //libera memoria do ponteiro
         sqlite3_finalize(stmt);
         return -1;
     }
@@ -93,6 +102,9 @@ int insertSensorData(const std::string& machineId, const std::string& sensorId, 
     return 0;
 }
 int insertAlarm(const std::string& machineId, const std::string& alarmType, const std::string& timestamp) {
+    if(alarmType !="inactive"){ 
+        std::cout << "ALARM: " + MACHINE_ID + ".alarms." + alarmType + "  TIME: " + timestamp << std::endl;
+    }
     std::string sql = "INSERT INTO alarms (machine_id, alarm_type, timestamp) VALUES (?, ?, ?);";
     sqlite3_stmt* stmt;
     
@@ -119,7 +131,6 @@ class DataProcessor {
 private:
     mqtt::async_client& client;
     std::map<std::string, SensorData> sensorDataMap;
-    AlarmManager alarmManager;
     std::mutex dataMutex;
 
 public:
@@ -127,8 +138,9 @@ public:
 
     // Função para processar os dados de temperatura e umidade
     void processSensorData(const std::string& sensorId, float value, const std::string& timestamp) {
-        std::cout << "processSensorData()" << std::endl;
-        std::cout<< "sensor ID: " << sensorId << "-  value: " << value << std::endl;
+        //std::cout << "processSensorData()" << std::endl;
+        //std::cout<< "sensor ID: " << sensorId << "-  value: " << value << std::endl;
+        //garante que apenas uma thread por vez acesse o dataMap
         std::lock_guard<std::mutex> lock(dataMutex);
         //std::string sensorId;
 
@@ -148,53 +160,59 @@ public:
         insertSensorData(MACHINE_ID, sensorId, value, timestamp);
         // Verifica se o valor do sensor está dentro da faixa ideal
         if (sensorId == "sensor_temperature") {
-            if (value > 20.0f && value < 40.0f) {
-                alarmManager.generateAlarm(MACHINE_ID + ".alarms.temperature");
-                insertAlarm(MACHINE_ID, "temperature", timestamp);
+            if (value > 20.0f && value < 26.0f) {
+                insertAlarm(MACHINE_ID, "good_temperature", timestamp);
+            }
+            if (value < 20.0f) {
+                insertAlarm(MACHINE_ID, "low_temperature", timestamp);
+            }
+            if (value > 26.0f) {
+                insertAlarm(MACHINE_ID, "high_temperature", timestamp);
             }
         } else if (sensorId == "sensor_humidity") {
-            if (value > 40.0f && value < 80.0f) {
-                alarmManager.generateAlarm(MACHINE_ID + ".alarms.humidity");
-                insertAlarm(MACHINE_ID, "humidity", timestamp);
+            if (value > 40.0f && value < 60.0f) {
+                insertAlarm(MACHINE_ID, "good_humidity", timestamp);
             }
-        }
-
-        // Processamento de inatividade
-        if (data.missed_periods >= 10) {
-            alarmManager.generateAlarm(MACHINE_ID + ".alarms.inactive");
+            if (value < 40.0f) {
+                insertAlarm(MACHINE_ID, "low_humidity", timestamp);
+            }
+            if (value > 60.0f) {
+                insertAlarm(MACHINE_ID, "high_humidity", timestamp);
+            }
         }
         
     }
 
     // Função para verificar sensores inativos
     void checkInactiveSensors() {
-        std::cout << "checkInactiveSensors()" << std::endl;
+        //std::cout << "checkInactiveSensors()" << std::endl;
         std::lock_guard<std::mutex> lock(dataMutex);
-
         for (auto& entry : sensorDataMap) {
             SensorData& data = entry.second;
             data.missed_periods++;
-
-            // Se o sensor estiver inativo por 10 períodos, gerar alarme
-            if (data.missed_periods >= 2) {
-                alarmManager.generateAlarm(MACHINE_ID + ".alarms.inactive");
+            //std::cout << entry.second.value << ": " << entry.second.missed_periods << std::endl;
+            // Se o sensor estiver inativo por 3 períodos, gerar alarme
+            if (data.missed_periods >= 3) {
+                std::cout << "ALARM: " + MACHINE_ID + ".alarms.inactive  SINCE: " + data.timestamp << std::endl;
                 insertAlarm(MACHINE_ID, "inactive", data.timestamp);
             }
-        }
-        
+        }   
     }
 
 };
 
 // Função para processar os dados JSON recebidos
 void processIncomingMessage(std::string& topic, const std::string& message, DataProcessor& processor) {
-    
+    //cria um objeto reader que constroi um parser json, que será utilizado para converter uma string em json value
     Json::CharReaderBuilder reader;
+    //estrutura de dados, objeto json generico para armazenar dados extraidos da string json, acessado como um map
     Json::Value root;
+    //converte a string message em um formato que pode ser lido sequencialmente, como se fosse um arquivo de entrada.
     std::istringstream s(message);
+    //armazena mensagem de erro
     std::string errs;
     std::string sensorId;
-    std::cout<< "topic: " << topic << "- message: " << message << std::endl;
+    //std::cout<< "topic: " << topic << "- message: " << message << std::endl;
     if(topic.find("sensor_temperature")!=std::string::npos){
         sensorId = "sensor_temperature";
         //std::cout << "sensor ID processIncoming1: " << sensorId << std::endl;
@@ -204,6 +222,7 @@ void processIncomingMessage(std::string& topic, const std::string& message, Data
         //std::cout << "sensor ID processIncoming2: " << sensorId << std::endl;
     }
     
+    //converte a string s em um objeto value root, reader configura o parser
     if (Json::parseFromStream(reader, s, &root, &errs)) {
         //std::cout << "anem id......"<< root["machine_id"].asString() << std::endl;
         //std::string sensorId;
@@ -211,7 +230,7 @@ void processIncomingMessage(std::string& topic, const std::string& message, Data
         std::string timestamp = root["timestamp"].asString();
         //std::cout << "processIncomingMessage() - " << sensorId<< std::endl;
         // Processa os dados do sensor
-        processor.processSensorData(sensorId, value, timestamp);
+        if(sensorId.size()> 3) processor.processSensorData(sensorId, value, timestamp);
     } else {
         std::cerr << "Error parsing JSON: " << errs << std::endl;
     }
@@ -220,11 +239,12 @@ void processIncomingMessage(std::string& topic, const std::string& message, Data
 
 // Função para processar os alarmes
 void processAlarms(DataProcessor& processor) {
-    std::cout << "processAlarms()" << std::endl;
+    //std::cout << "processAlarms()" << std::endl;
     processor.checkInactiveSensors();
 }
 
-// Classe que implementa a interface mqtt::callback
+// Subclasse que implementa a interface mqtt::callback (interface de retorno 
+// de chamada do cliente mqtt para processar eventos, mensagens, conexão, etc)
 class CallbackHandler : public mqtt::callback {
 private:
     DataProcessor& processor;
@@ -232,17 +252,20 @@ private:
 public:
     CallbackHandler(DataProcessor& proc) : processor(proc) {}
 
+    // chamado automaticamente quando uma mensagem (ponteiro msg para a mensagem) é recebida
     void message_arrived(mqtt::const_message_ptr msg) {
-        std::cout << "message_arrived()" << std::endl;
+        //std::cout << "message_arrived()" << std::endl;
+        //tópico ao qual a mensagem foi publicada
         std::string topic = msg->get_topic();
         //std::cout <<"topic: "<< topic << std::endl;
-        std::string payload = msg->get_payload_str();
-        processIncomingMessage(topic, payload, processor);   
+        // conteúdo da mensagem
+        std::string message = msg->get_payload_str();
+        processIncomingMessage(topic, message, processor);   
     }
 };
 
 int main(int argc, char* argv[]) {
-    // Conectar ao banco de dados SQLite
+    // abrir/criar um aqrquivo de banco de dados 
     if (sqlite3_open("sensor_data.db", &db) != SQLITE_OK) {
         std::cerr << "Error opening SQLite database: " << sqlite3_errmsg(db) << std::endl;
         return -1;
@@ -253,10 +276,13 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     
-    
+    //inicializa o cliente mqtt assíncrono
     mqtt::async_client client(SERVER_ADDRESS, CLIENT_ID);
+    //objeto de opções de conexão para configurar a conexão entre o client e o broker mqtt
     mqtt::connect_options connOpts;
+    //inicia a conexão com o broker mqtt
     mqtt::token_ptr conntok = client.connect(connOpts);
+    //bloqueia a execução até estabelezar a conexão
     conntok->wait();
     //std::cout << "conectou" << std::endl;
     DataProcessor processor(client);
@@ -264,10 +290,10 @@ int main(int argc, char* argv[]) {
     // Instanciando o callback handler
     CallbackHandler callbackHandler(processor);
 
-    // Definindo o callback do cliente
+    // Definindo o callback do cliente para lidar com eventos do mqtt
     client.set_callback(callbackHandler);
 
-    // Inscrevendo-se nos tópicos
+    // Inscrevendo-se nos tópicos (1-recebe pelo menos uma vez)
     client.subscribe("/sensor_monitors", 1)->wait();
     client.subscribe("/sensors/" + MACHINE_ID + "/sensor_temperature", 1)->wait();
     client.subscribe("/sensors/" + MACHINE_ID + "/sensor_humidity", 1)->wait();
